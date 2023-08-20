bf04e6d4db73a634a98316e4314b88edf072267f
IrisWRX <wxu64@my.bcit.ca>
2023-08-20 16:20:11 -0700

    controlled the state of input and select fields

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -21,18 +23,34 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     // avoid reloading
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     // submit event happens if button is clicked or enter is hit
     // usually involves a brief flash and page reload
     // onSubmit include click and enter while onClick only deals with click
-    <div className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-      <select>
+      {/* e.target.value comes from the value={num} in <option> */}
+      <select
+        value={quantity}
+        // to get the number instead of a string
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {/* (_, i) is like a map function. inside it, _ is the current value, i is the index */}
         {/* when rendering a list, need to give each element a unique key */}
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
@@ -41,12 +59,18 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>ADD</button>
-    </div>
+    </form>
   );
 }
 
+// cannot pass props from form into packinglist because they're sibling components
 function PackingList() {
   return (
     <div className="list">
