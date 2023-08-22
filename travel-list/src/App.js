26d59ee66a06a6f02b325c430b8ff3289d6c47ca
IrisWRX <wxu64@my.bcit.ca>
2023-08-21 21:21:50 -0700

    calculated statistics on footer using derived state

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -118,10 +118,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  // dirived state
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have x items on your list, and you already packed x.</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go 🛫"
+          : `You have ${numItems} items on your list, and you already packed ${" "}
+        ${numPacked} (${percentage}%).`}
+      </em>
     </footer>
   );
 }
